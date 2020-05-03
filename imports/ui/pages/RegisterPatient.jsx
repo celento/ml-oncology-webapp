b026e7672c8f69659261365d15fb15796173ccd6
Celento C George <celentocg@live.com>
2020-05-03 19:23:11 +0530

    mobile register ui

--- imports/ui/pages/RegisterPatient.jsx
+++ imports/ui/pages/RegisterPatient.jsx
@@ -11,14 +11,19 @@ import { PropagateLoader} from 'react-spinners';
 import { css } from 'react-emotion';
 import { Button, notification } from 'antd';
 import {ExclamationCircleOutlined} from '@ant-design/icons';
+import { Radio } from 'antd';
+import { Select } from 'antd';
+import { Input } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 
-
+const { Option } = Select;
 const override = css`
     display: block;
     border-color: red;
 `;
 
 
+ 
 
 var state,url;
 
@@ -29,8 +34,12 @@ class RegisterPatient extends Component {
     
     this.state = {
       error: '',
-      s1:"show_div",
-      s2:"hide_div"
+      s1:"animation_div",
+      s2:"animation_div anim_hide",
+      s3:"animation_div anim_hide",
+      s4:"animation_div anim_hide",
+      s5:"animation_div anim_hide",
+
     };
  
   
@@ -38,17 +47,20 @@ class RegisterPatient extends Component {
 
 
     // Steps
-
+    this.stepOne = this.stepOne.bind(this);
     this.stepTwo = this.stepTwo.bind(this);
     this.stepThree = this.stepThree.bind(this);
     this.stepFour = this.stepFour.bind(this);
     this.stepFive = this.stepFive.bind(this);
-    this.stepSix= this.stepSix.bind(this);
-    this.submit= this.submit.bind(this);
+  
 
     
 
     this.openNotification = this.openNotification.bind(this);
+
+    this.onChangeAge=this.onChangeAge.bind(this);
+    this.onChangeGender=this.onChangeGender.bind(this);
+
 
 
   }
@@ -65,130 +77,63 @@ class RegisterPatient extends Component {
     });
   };
 
+// Welcome
+  stepOne(){
+    // Animation 
+    this.setState({
+      s1: "animation_div anim_hide",
+      s2:"animation_div"
+    })
   
+  }
+
+// Name
   stepTwo(){
-    console.log("hellow wo")
-
-    // this.setState({
-    //   s1:"anim_hide_div",
-    //   s2:"anim_show_div"
-    // })
-
-    var el = document.getElementById('step1');
-    console.log(el)
-
-    for (let i=0; i<=500; i+=5) { 
-      task(i); 
-     
-   } 
-
- 
-     
-   function task(i) { 
-     if(i==499){
-       el.style.display="none";
-     }
-     setTimeout(function() { 
-        el.style.marginTop = -i+"px";
-        el.style.opacity = Math.round((1-i/500) * 10) / 10
-         console.log(i,Math.round((1-i/500) * 10) / 10)
-     }, 1/1.5*i); 
-   }   
- 
-    
+  
+    // Data Store
+    this.setState({
+      name: document.getElementById('g_name').value,
+    })
+    // Animation 
+    this.setState({
+      s2:"animation_div anim_hide",
+      s3:"animation_div"
+    })
   }
 
+// Age + Gener
   stepThree(){
+    // Animation 
     this.setState({
-      s1:"anim_show_div",
-      s2:"anim_hide_div"
+      s3:"animation_div anim_hide",
+      s4:"animation_div"
     })
-
   }
 
 
+// Mobile Number
   stepFour(){
-    // name,age,gender,phone
-    var taluk = document.getElementById('taluk').value;
-    var panchayat = document.getElementById('panchayat').value;
-    var ward = document.getElementById('ward').value;
-  
-
-    if(taluk==""||panchayat==""||ward==""){
-      this.openNotification();
-    }else{
     this.setState({
-      stepThree:"disp_false",
-      stepFour:"disp_true",
-      taluk,panchayat,ward
+      mobile:document.getElementById('g_mobile').value,
     })
-    }
 
-    console.log(this.state.pincode)
-  }
-
-
-
-  stepFive(){
-    // name,age,gender,phone
-    var travel = document.getElementById('travel').value;
-    var country = document.getElementById('country').value;
-  
-
-    if(travel=="yes" && country=="No"){
+      // Animation 
       this.setState({
-        error:"Please select a country",
+        s4:"animation_div anim_hide",
+        s5:"animation_div"
       })
-      this.openNotification();
-    }else{
-    this.setState({
-      stepFour:"disp_false",
-      stepFive:"disp_true",
-      travel,country
-    })
-    }
-    console.log(this.state.pincode)
   }
 
   
-
-  stepSix(){
-    // name,age,gender,phone
-    var medical = document.getElementById('medical').value;
-
-    if(medical==""){
-      this.openNotification();
-    }else{
-    this.setState({
-      stepFive:"disp_false",
-      stepSix:"disp_true",
-      medical
+// Finish
+  stepFive(){
+     // Animation 
+     this.setState({
+      s4:"animation_div anim_hide",
+      s5:"animation_div"
     })
-    }
-    console.log(this.state.pincode)
-  }
 
-  submit(){
-    console.log(this.state)
 
-    var name = this.state.name;
-    var age = this.state.age;
-    var gender = this.state.gender;
-    var phone = this.state.phone;
-
-    var house = this.state.house;
-    var street = this.state.street;
-    var pincode = this.state.pincode;
-
-    var taluk = this.state.taluk;
-    var panchayat = this.state.panchayat;
-    var ward = this.state.ward;
-
-    var travel = this.state.travel;
-    var country = this.state.country;
-    var medical = this.state.medical;
-    
-    var categoryID = taluk+panchayat+ward;
 
     var userid = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -199,62 +144,34 @@ class RegisterPatient extends Component {
     userno = userid;
     console.log(userid)
 
-    Meteor.call('create_patient',userid,categoryID,name,age,gender,phone,house,street,pincode,taluk,panchayat,ward,travel,country,medical,(err)=>{
-      if(!err){
-        this.props.history.push('/p/xcv/home?id='+userid);
-    
-      }
-    }).bind(this)
-  
+
   }
 
-   
-  handleSubmit(e){
-     
-    // e.preventDefault();
-   
-    let name = document.getElementById("normal_signup_name").value;
-    let email = document.getElementById("normal_signup_email").value;
-    let password = document.getElementById("normal_signup_password").value;
-    let retype = document.getElementById("normal_signup_password-repeat").value;
-    let hospital = document.getElementById("normal_signup_hospital").value;
-    let license = document.getElementById("normal_signup_license").value;
-    let type = "doctor"
-   
-    console.log(name,email,password,retype,hospital,license)
 
-    var userid = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    
-    for (var i = 0; i < 15; i++)
-    userid += possible.charAt(Math.floor(Math.random() * possible.length));
-
-    
-    if (password !=retype) {
-      
-      alert("Passwords do not match");
-
-    }else{
-
-            Meteor.call('CREATE_NEW_ACCOUNT_DOCTOR',name,email,password,userid,hospital,license,type,(error)=>{
-              if(error){
-               alert(error)
-              }else{
-                Meteor.loginWithPassword(email, password, (err) => {
-                  if(err){
-                     alert("Incorrect email or password")
-                  } else { 
-                      this.props.history.push('/d/start');
-                  }
-                });
-              }
-
-            });
+  onChangeGender(e) {
+    this.setState({
+      gender:e.target.value,
+    })
+    console.log(`radio checked:${e.target.value}`);
+  }
+  onChangeAge(value) {
+    this.setState({
+      age:value,
+    })  
+    console.log(`selected ${value}`);
+  }
  
-    }
-}
+   
+ 
 
   render(){
+
+    var ageGenerator = []
+    for(var i=1;i<120;i++){
+      var x = <Option value={i}>{i}</Option>
+      ageGenerator.push(x)
+    }
+    
 
     <Helmet>
     <meta charSet="utf-8" />
@@ -268,35 +185,104 @@ class RegisterPatient extends Component {
     return (
       <div className="ans-bg">
         <Notifications/>
-        <div className="answer_container">
-           
 
-           <div id="step1" className={"anim_div " + this.state.s1}>
+        {/* Welcome */}
+        <div className={"answer_container "+ this.state.s1}>
+        
+           <div id="step1">
              <center>
              <img src="/img/icon_m.png" className="ans_logo"/>
 
              <h1 className="ans_title"> <b>Welcome</b></h1>
               <p className="ans_content"> Tap Get Started to register yourself and book an appointment with a doctor around you</p>
 
-              <Button onClick={this.stepTwo} className="getStartedbtn" type="primary">Get Started</Button>
+              <Button onClick={this.stepOne} className="getStartedbtn" type="primary">Get Started</Button>
              </center>
           
            </div>
+      </div>
+
+      {/* Name */}
+      <div className={"answer_container "+ this.state.s2}>
+
+        <div id="step2">
+          <center>
+          <img src="/img/icon_m.png" className="ans_logo"/>
+
+          <p className="ans_content">Name</p>
+    
+          <Input id="g_name" size="large" placeholder="Full Name" prefix={<UserOutlined />} />
+
+          <Button onClick={this.stepTwo} className="getStartedbtn" type="primary">Next</Button>
+          </center>
+
+        </div>
+
+        </div>
 
 
-           <div id="step2" className={"anim_div " + this.state.s2}>
+      {/* Gender & Age */}
+      <div className={"answer_container "+ this.state.s3}>
+
+           <div id="step3">
              <center>
              <img src="/img/icon_m.png" className="ans_logo"/>
+ 
+              <p className="ans_content">Gender & Age</p>
+              <Radio.Group size="large" onChange={this.onChangeGender} defaultValue="Male">
+                <Radio.Button value="Male">Male</Radio.Button>
+                <Radio.Button value="Female">Female</Radio.Button>
+              </Radio.Group> &nbsp;&nbsp;
+              <Select size="large" defaultValue="Age" style={{ width: 120 }} onChange={this.onChangeAge}>
+                {ageGenerator}
+              </Select>
 
-             <h1 className="ans_title"> <b>Hello</b></h1>
-              <p className="ans_content"> Tap Get Started to register yourself and book an appointment with a doctor around you</p>
-
-              <Button onClick={this.stepThree} className="getStartedbtn" type="primary">Get Started</Button>
+              <Button onClick={this.stepThree} className="getStartedbtn" type="primary">Next</Button>
              </center>
           
            </div>
 
         </div>
+
+        {/* Mobile Number */}
+        <div className={"answer_container "+ this.state.s4}>
+
+          <div id="step4">
+            <center>
+            <img src="/img/icon_m.png" className="ans_logo"/>
+
+            <p className="ans_content">Mobile Number</p>
+
+            <Input id="g_mobile" size="large" placeholder="Enter you Mobile Number" prefix={<UserOutlined />} />
+
+            <Button onClick={this.stepFour} className="getStartedbtn" type="primary">Next</Button>
+            </center>
+
+          </div>
+
+      </div>
+
+
+       {/* Finish */}
+       <div className={"answer_container "+ this.state.s5}>
+        
+        <div id="step1">
+          <center>
+          <img src="/img/icon_m.png" className="ans_logo"/>
+
+          <h1 className="ans_title"> <b>Finish</b></h1>
+           <p className="ans_content"> You have successfully created the</p>
+
+           <Button onClick={this.stepOne} className="getStartedbtn" type="primary">Get Started</Button>
+          </center>
+       
+        </div>
+       </div>
+
+
+
+
+
       </div>
        
       
