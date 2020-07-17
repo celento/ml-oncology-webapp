0d4912f1a6e50d3df4751323db84d668e7bf4af5
Celento C George <celentocg@live.com>
2020-07-17 22:26:07 +0530

    Skin Cancer

---
+++ Training/Skin Cancer.py
@@ -0,0 +1,505 @@
+from numpy.random import seed
+seed(101)
+from tensorflow import set_random_seed
+set_random_seed(101)
+
+import pandas as pd
+import numpy as np
+#import keras
+#from keras import backend as K
+
+import tensorflow
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+
+import os
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+import itertools
+import shutil
+import matplotlib.pyplot as plt
+ 
+
+# Create a new directory
+base_dir = 'base_dir'
+os.mkdir(base_dir)
+ 
+
+# train_dir
+    # nv
+    # mel
+    # bkl
+    # bcc
+    # akiec
+    # vasc
+    # df
+ 
+# val_dir
+    # nv
+    # mel
+    # bkl
+    # bcc
+    # akiec
+    # vasc
+    # df
+ 
+train_dir = os.path.join(base_dir, 'train_dir')
+os.mkdir(train_dir)
+
+ 
+val_dir = os.path.join(base_dir, 'val_dir')
+os.mkdir(val_dir)
+
+# create new folders inside train_dir
+nv = os.path.join(train_dir, 'nv')
+os.mkdir(nv)
+mel = os.path.join(train_dir, 'mel')
+os.mkdir(mel)
+bkl = os.path.join(train_dir, 'bkl')
+os.mkdir(bkl)
+bcc = os.path.join(train_dir, 'bcc')
+os.mkdir(bcc)
+akiec = os.path.join(train_dir, 'akiec')
+os.mkdir(akiec)
+vasc = os.path.join(train_dir, 'vasc')
+os.mkdir(vasc)
+df = os.path.join(train_dir, 'df')
+os.mkdir(df)
+
+# create new folders inside val_dir
+nv = os.path.join(val_dir, 'nv')
+os.mkdir(nv)
+mel = os.path.join(val_dir, 'mel')
+os.mkdir(mel)
+bkl = os.path.join(val_dir, 'bkl')
+os.mkdir(bkl)
+bcc = os.path.join(val_dir, 'bcc')
+os.mkdir(bcc)
+akiec = os.path.join(val_dir, 'akiec')
+os.mkdir(akiec)
+vasc = os.path.join(val_dir, 'vasc')
+os.mkdir(vasc)
+df = os.path.join(val_dir, 'df')
+os.mkdir(df)
+
+
+df_data = pd.read_csv('input/HAM10000_metadata.csv')
+
+# df_data.head()
+ 
+df = df_data.groupby('lesion_id').count()
+
+def identify_duplicates(x):
+    
+    unique_list = list(df['lesion_id'])
+    
+    if x in unique_list:
+        return 'no_duplicates'
+    else:
+        return 'has_duplicates'
+    
+# create a new colum that is a copy of the lesion_id column
+df_data['duplicates'] = df_data['lesion_id']
+# apply the function to this new column
+df_data['duplicates'] = df_data['duplicates'].apply(identify_duplicates)
+
+# df_data.head()
+
+
+df_data['duplicates'].value_counts()
+
+
+# filter out images that don't have duplicates
+df = df_data[df_data['duplicates'] == 'no_duplicates']
+
+df.shape
+
+
+_, df_val = train_test_split(df, test_size=0.17, random_state=101)
+
+df_val.shape
+
+ 
+# This function identifies if an image is part of the train
+# or val set.
+def identify_val_rows(x):
+    # create a list of all the lesion_id's in the val set
+    val_list = list(df_val['image_id'])
+    
+    if str(x) in val_list:
+        return 'val'
+    else:
+        return 'train'
+
+# identify train and val rows
+
+# create a new colum that is a copy of the image_id column
+df_data['train_or_val'] = df_data['image_id']
+
+# apply the function to this new column
+df_data['train_or_val'] = df_data['train_or_val'].apply(identify_val_rows)
+   
+# filter out train rows
+df_train = df_data[df_data['train_or_val'] == 'train']
+
+ 
+print(len(df_train))
+print(len(df_val))
+ 
+# Set the image_id as the index in df_data
+df_data.set_index('image_id', inplace=True)
+
+
+
+# Get a list of images in each of the two folders
+folder_1 = os.listdir('input/ham10000_images_part_1')
+folder_2 = os.listdir('input/ham10000_images_part_2')
+
+# Get a list of train and val images
+train_list = list(df_train['image_id'])
+val_list = list(df_val['image_id'])
+
+
+
+# Transfer the train images
+
+for image in train_list:
+    
+    fname = image + '.jpg'
+    label = df_data.loc[image,'dx']
+    
+    if fname in folder_1:
+        
+        src = os.path.join('input/ham10000_images_part_1', fname)
+        
+        dst = os.path.join(train_dir, label, fname)
+       
+        shutil.copyfile(src, dst)
+
+    if fname in folder_2:
+        
+        src = os.path.join('input/ham10000_images_part_2', fname)
+    
+        dst = os.path.join(train_dir, label, fname)
+        
+        shutil.copyfile(src, dst)
+
+
+# Transfer the val images
+
+for image in val_list:
+    
+    fname = image + '.jpg'
+    label = df_data.loc[image,'dx']
+    
+    if fname in folder_1:
+        
+        src = os.path.join('input/ham10000_images_part_1', fname)
+       
+        dst = os.path.join(val_dir, label, fname)
+       
+        shutil.copyfile(src, dst)
+
+    if fname in folder_2:
+       
+        src = os.path.join('input/ham10000_images_part_2', fname)
+        
+        dst = os.path.join(val_dir, label, fname)
+      
+        shutil.copyfile(src, dst)
+
+
+print(len(os.listdir('base_dir/train_dir/nv')))
+print(len(os.listdir('base_dir/train_dir/mel')))
+print(len(os.listdir('base_dir/train_dir/bkl')))
+print(len(os.listdir('base_dir/train_dir/bcc')))
+print(len(os.listdir('base_dir/train_dir/akiec')))
+print(len(os.listdir('base_dir/train_dir/vasc')))
+print(len(os.listdir('base_dir/train_dir/df')))
+
+print(len(os.listdir('base_dir/val_dir/nv')))
+print(len(os.listdir('base_dir/val_dir/mel')))
+print(len(os.listdir('base_dir/val_dir/bkl')))
+print(len(os.listdir('base_dir/val_dir/bcc')))
+print(len(os.listdir('base_dir/val_dir/akiec')))
+print(len(os.listdir('base_dir/val_dir/vasc')))
+print(len(os.listdir('base_dir/val_dir/df')))
+
+# not augmenting class 'nv' (inorder to keep the ratio)
+class_list = ['mel','bkl','bcc','akiec','vasc','df']
+
+for item in class_list:
+    
+    aug_dir = 'aug_dir'
+    os.mkdir(aug_dir)
+     
+    img_dir = os.path.join(aug_dir, 'img_dir')
+    os.mkdir(img_dir)
+
+    # Choose a class
+    img_class = item
+
+    img_list = os.listdir('base_dir/train_dir/' + img_class)
+
+     
+    for fname in img_list:
+      
+            src = os.path.join('base_dir/train_dir/' + img_class, fname)
+      
+            dst = os.path.join(img_dir, fname) 
+
+            shutil.copyfile(src, dst)
+
+
+     
+    path = aug_dir
+    save_path = 'base_dir/train_dir/' + img_class
+
+    # Create a data generator
+    datagen = ImageDataGenerator(
+        rotation_range=180,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        zoom_range=0.1,
+        horizontal_flip=True,
+        vertical_flip=True,
+        #brightness_range=(0.9,1.1),
+        fill_mode='nearest')
+
+    batch_size = 50
+
+    aug_datagen = datagen.flow_from_directory(path,
+                                           save_to_dir=save_path,
+                                           save_format='jpg',
+                                                    target_size=(224,224),
+                                                    batch_size=batch_size)
+
+
+
+     
+    
+    num_aug_images_wanted = 6000 # total number of images we want to have in each class
+     
+    
+    num_files = len(os.listdir(img_dir))
+    num_batches = int(np.ceil((num_aug_images_wanted-num_files)/batch_size))
+ 
+    for i in range(0,num_batches):
+
+        imgs, labels = next(aug_datagen)
+        
+    # delete temporary directory with the raw image files
+    shutil.rmtree('aug_dir')
+
+ 
+
+print(len(os.listdir('base_dir/train_dir/nv')))
+print(len(os.listdir('base_dir/train_dir/mel')))
+print(len(os.listdir('base_dir/train_dir/bkl')))
+print(len(os.listdir('base_dir/train_dir/bcc')))
+print(len(os.listdir('base_dir/train_dir/akiec')))
+print(len(os.listdir('base_dir/train_dir/vasc')))
+print(len(os.listdir('base_dir/train_dir/df')))
+ 
+
+print(len(os.listdir('base_dir/val_dir/nv')))
+print(len(os.listdir('base_dir/val_dir/mel')))
+print(len(os.listdir('base_dir/val_dir/bkl')))
+print(len(os.listdir('base_dir/val_dir/bcc')))
+print(len(os.listdir('base_dir/val_dir/akiec')))
+print(len(os.listdir('base_dir/val_dir/vasc')))
+print(len(os.listdir('base_dir/val_dir/df')))
+
+ 
+def plots(ims, figsize=(12,6), rows=5, interp=False, titles=None): # 12,6
+    if type(ims[0]) is np.ndarray:
+        ims = np.array(ims).astype(np.uint8)
+        if (ims.shape[-1] != 3):
+            ims = ims.transpose((0,2,3,1))
+    f = plt.figure(figsize=figsize)
+    cols = len(ims)//rows if len(ims) % 2 == 0 else len(ims)//rows + 1
+    for i in range(len(ims)):
+        sp = f.add_subplot(rows, cols, i+1)
+        sp.axis('Off')
+        if titles is not None:
+            sp.set_title(titles[i], fontsize=16)
+        plt.imshow(ims[i], interpolation=None if interp else 'none')
+        
+plots(imgs, titles=None)  
+
+
+ 
+
+train_path = 'base_dir/train_dir'
+valid_path = 'base_dir/val_dir'
+
+num_train_samples = len(df_train)
+num_val_samples = len(df_val)
+train_batch_size = 10
+val_batch_size = 10
+image_size = 224
+
+train_steps = np.ceil(num_train_samples / train_batch_size)
+val_steps = np.ceil(num_val_samples / val_batch_size)
+
+
+
+datagen = ImageDataGenerator(
+    preprocessing_function= \
+    tensorflow.keras.applications.mobilenet.preprocess_input)
+
+train_batches = datagen.flow_from_directory(train_path,
+                                            target_size=(image_size,image_size),
+                                            batch_size=train_batch_size)
+
+valid_batches = datagen.flow_from_directory(valid_path,
+                                            target_size=(image_size,image_size),
+                                            batch_size=val_batch_size)
+ 
+test_batches = datagen.flow_from_directory(valid_path,
+                                            target_size=(image_size,image_size),
+                                            batch_size=1,
+                                            shuffle=False)
+
+
+mobile = tensorflow.keras.applications.mobilenet.MobileNet()
+
+mobile.summary()
+
+type(mobile.layers)
+len(mobile.layers)
+ 
+predictions = Dense(7, activation='softmax')(x)
+ 
+
+model = Model(inputs=mobile.input, outputs=predictions)
+
+
+model.summary()
+
+ 
+
+from tensorflow.keras.metrics import categorical_accuracy, top_k_categorical_accuracy
+
+def top_3_accuracy(y_true, y_pred):
+    return top_k_categorical_accuracy(y_true, y_pred, k=3)
+
+def top_2_accuracy(y_true, y_pred):
+    return top_k_categorical_accuracy(y_true, y_pred, k=2)
+
+model.compile(Adam(lr=0.01), loss='categorical_crossentropy', 
+          metrics=[categorical_accuracy, top_2_accuracy, top_3_accuracy])
+
+ 
+print(valid_batches.class_indices)
+
+
+# Add weights to try to make the model more sensitive to melanoma 
+
+class_weights={
+    0: 1.0, # akiec
+    1: 1.0, # bcc
+    2: 1.0, # bkl
+    3: 1.0, # df
+    4: 3.0, # mel # Try to make the model more sensitive to Melanoma.
+    5: 1.0, # nv
+    6: 1.0, # vasc
+}
+
+
+filepath = "model.h5"
+checkpoint = ModelCheckpoint(filepath, monitor='accuracy', verbose=1, 
+                             save_best_only=True, mode='max')
+
+reduce_lr = ReduceLROnPlateau(monitor='accuracy', factor=0.5, patience=2, 
+                                   verbose=1, mode='max', min_lr=0.00001)
+                              
+                              
+callbacks_list = [checkpoint, reduce_lr]
+
+history = model.fit_generator(train_batches, steps_per_epoch=train_steps, 
+                              class_weight=class_weights,
+                    validation_data=valid_batches,
+                    validation_steps=val_steps,
+                    epochs=30, verbose=1,
+                   callbacks=callbacks_list)
+
+ 
+
+val_loss, val_cat_acc, val_top_2_acc, val_top_3_acc = \
+model.evaluate_generator(test_batches, 
+                        steps=len(df_val))
+
+print('val_loss:', val_loss)
+print('val_cat_acc:', val_cat_acc)
+print('val_top_2_acc:', val_top_2_acc)
+print('val_top_3_acc:', val_top_3_acc)
+
+ 
+
+model.load_weights('model.h5')
+
+val_loss, val_cat_acc, val_top_2_acc, val_top_3_acc = \
+model.evaluate_generator(test_batches, 
+                        steps=len(df_val))
+
+print('val_loss:', val_loss)
+print('val_cat_acc:', val_cat_acc)
+print('val_top_2_acc:', val_top_2_acc)
+print('val_top_3_acc:', val_top_3_acc)
+
+
+ 
+
+import matplotlib.pyplot as plt
+
+acc = history.history['categorical_accuracy']
+val_acc = history.history['val_categorical_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+train_top2_acc = history.history['top_2_accuracy']
+val_top2_acc = history.history['val_top_2_accuracy']
+train_top3_acc = history.history['top_3_accuracy']
+val_top3_acc = history.history['val_top_3_accuracy']
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.figure()
+
+plt.plot(epochs, acc, 'bo', label='Training cat acc')
+plt.plot(epochs, val_acc, 'b', label='Validation cat acc')
+plt.title('Training and validation cat accuracy')
+plt.legend()
+plt.figure()
+
+
+plt.plot(epochs, train_top2_acc, 'bo', label='Training top2 acc')
+plt.plot(epochs, val_top2_acc, 'b', label='Validation top2 acc')
+plt.title('Training and validation top2 accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, train_top3_acc, 'bo', label='Training top3 acc')
+plt.plot(epochs, val_top3_acc, 'b', label='Validation top3 acc')
+plt.title('Training and validation top3 accuracy')
+plt.legend()
+
+
+plt.show()
+
+
+ 
+
+test_labels = test_batches.classes
+
+
+predictions = model.predict_generator(test_batches, steps=len(df_val), verbose=1)
+predictions.shape
+ 
